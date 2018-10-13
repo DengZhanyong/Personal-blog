db790ae107b7af1707a76f2bd8616473e3f83664
deng123 <2657179843@qq.com>
2018-10-13 16:50:21 +0800

    改进小作品展示界面

--- otherpages/ExhibitionOfWorks/worksdata.js
+++ otherpages/ExhibitionOfWorks/worksdata.js
@@ -1,45 +1,60 @@
 var works = [
-	{
-		title:"Canvas类",
-		id:"",
-		desc:"利用HTML 的 Canvas制作的一些小作品，包括特效、游戏等",
-		contents:[
-			{
-				work_name: '转盘抽奖(移动端)',
-            	describe: '为庆祝与女朋友相处两周年，为她制作的一个专属抽奖活动。',
-				date:'',
-            	id: '2018-9-17',
-            	src: 'http://www.xmwweb.cn/dzy/ToHxy/TwoYears/'
+    {
+        title: "Canvas类",
+        id: "",
+        desc: "利用HTML 的 Canvas制作的一些小作品，包括特效、游戏等",
+        contents: [
+            {
+                work_name: '转盘抽奖(移动端)',
+                describe: '为庆祝与女朋友相处两周年，为她制作的一个专属抽奖活动。',
+                date: '',
+                id: '2018-9-17',
+                src: 'http://www.xmwweb.cn/dzy/ToHxy/TwoYears/'
 			}
-        , 
-			{
-				work_name: '绚丽时钟(PC端)',
-            	describe: '利用HTML Canvas制作的一个绚丽时钟效果，目前只有两个功能(显示当前时间和定时器)，在时钟界面按ESC键退出时钟界面。',
-            	id: 'ffff',
-				date:'2018-9-16',
-            	src: 'http://www.xmwweb.cn/dzy/Canvas/clock'
+        ,
+            {
+                work_name: '绚丽时钟(PC端)',
+                describe: '利用HTML Canvas制作的一个绚丽时钟效果，目前只有两个功能(显示当前时间和定时器)，在时钟界面按ESC键退出时钟界面。',
+                id: 'ffff',
+                date: '2018-9-16',
+                src: 'http://www.xmwweb.cn/dzy/Canvas/clock'
 			}
         ]
 	},
-	{
-		title:"游戏类",
-		id:"",
-		desc:"主要是一些常见的小型游戏，目前还不具备担任开发大型游戏的能力，正在学习中",
-		contents:[
-			{
-            	work_name: '图书馆信息网(PC版)',
-            	describe: '初学时只利用了HTML,CSS,JS,jQuery制作的，此作品也是我个人用来参加学校网页设计大赛的作品',
-            	id: '2018.5.20',
-				date:'2018-5-20',
-            	src: 'http://www.xmwweb.cn/dzy/ctbuLibrary/'
-            },
+    {
+        title: "游戏类",
+        id: "",
+        desc: "主要是一些常见的小型游戏，目前还不具备担任开发大型游戏的能力，正在学习中",
+        contents: [
             {
-            	work_name: '扫雷',
-            	describe: '利用angularJS框架实现的一个网页版扫雷，可以自定义雷区大小和地雷数量。',
-				date:'',
-            	id: '2018-8-1',
-            	src: 'http://www.xmwweb.cn/dzy/game/Minesweeper/'
-            }, 
+                work_name: '扫雷',
+                describe: '利用angularJS框架实现的一个网页版扫雷，可以自定义雷区大小和地雷数量。',
+                date: '',
+                id: '2018-8-1',
+                src: 'http://www.xmwweb.cn/dzy/game/Minesweeper/'
+            }
+		]
+	},
+    {
+        title: "其他类",
+        id: "",
+        desc: "暂无说明",
+        contents: [
+            {
+                work_name: '图书馆信息网(PC版)',
+                describe: '初学时只利用了HTML,CSS,JS,jQuery制作的，此作品也是我个人用来参加学校网页设计大赛的作品',
+                id: '2018.5.20',
+                date: '2018-5-20',
+                src: 'http://www.xmwweb.cn/dzy/ctbuLibrary/'
+            }
+		]
+	},
+    {
+        title: "正在学习中",
+        id: "",
+        desc: "暂无说明",
+        contents: [
+
 		]
 	}
 ];
