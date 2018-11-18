aff3e52f1212a1c6acc97d3b71cc68e317ff3e4b
DengZy <2657179843@qq.com>
2018-11-18 12:23:49 +0800

    每个作品增加github链接

--- otherpages/ExhibitionOfWorks/worksdata.js
+++ otherpages/ExhibitionOfWorks/worksdata.js
@@ -6,6 +6,7 @@ var works = [
         contents: [
             {
                 work_name: '转盘抽奖(移动端)',
+				githubSrc:'https://github.com/CTBUdzy/HTML-Canvas',
                 describe: '为庆祝与女朋友相处两周年，为她制作的一个专属抽奖活动。',
                 date: '',
                 id: '2018-9-17',
@@ -14,6 +15,7 @@ var works = [
         ,
             {
                 work_name: '绚丽时钟(PC端)',
+				githubSrc:'https://github.com/CTBUdzy/HTML-Canvas',
                 describe: '利用HTML Canvas制作的一个绚丽时钟效果，目前只有两个功能(显示当前时间和定时器)，在时钟界面按ESC键退出时钟界面。',
                 id: 'ffff',
                 date: '2018-9-16',
@@ -28,6 +30,7 @@ var works = [
         contents: [
             {
                 work_name: '扫雷',
+				githubSrc:'',
                 describe: '利用angularJS框架实现的一个网页版扫雷，可以自定义雷区大小和地雷数量。',
                 date: '2018-8-1',
                 id: '2018-8-1',
@@ -42,10 +45,19 @@ var works = [
         contents: [
             {
                 work_name: '图书馆信息网(PC版)',
+				githubSrc:'https://github.com/CTBUdzy/library',
                 describe: '初学时只利用了HTML,CSS,JS,jQuery制作的，此作品也是我个人用来参加学校网页设计大赛的作品',
                 id: '2018.5.20',
                 date: '2018-5-20',
                 src: 'http://www.xmwweb.cn/dzy/ctbuLibrary/'
+            },
+			{
+                work_name: '高仿饿了么商家模块',
+				githubSrc:'https://github.com/CTBUdzy/elm-vue',
+                describe: '利用VUE+webpack搭建的一个项目，涉及到vue-router、webpack配置、stylus等技术',
+                id: '',
+                date: '',
+                src: ''
             }
 		]
 	},
@@ -56,6 +68,7 @@ var works = [
         contents: [
             {
                 work_name: '个人简历',
+				githubSrc:'https://github.com/CTBUdzy/Personal-blog',
                 describe: '利用HTML,CSS,JS,JQ制作的一个网页版个人简历，重点放在动画特效方面的制作，正在逐步构想动画效果',
                 id: '',
                 date: '2018-10-18',
@@ -63,6 +76,7 @@ var works = [
             },
             {
                 work_name: '可视化布局编辑器',
+				githubSrc:'https://github.com/CTBUdzy/Rapid-developmen-tools',
                 describe: '完全独立制作的一个可视化布局编辑器，可快速实现前端布局的，提高开发效率，目前已完成基础布局和组件的自定义和浏览功能',
                 id: '',
                 date: '2018-10-19',
@@ -70,7 +84,8 @@ var works = [
             },
             {
                 work_name: '2048',
-                describe: '自己制作的一个2048游戏，目前只是初版本',
+				githubSrc:'https://github.com/CTBUdzy/HTML-Canvas',
+                describe: '利用基础的HTML/css/js/jq制作的一个2048游戏，目前只是初版本',
                 date: '2018-10-15',
                 id: '2018-8-1',
                 src: 'http://www.xmwweb.cn/dzy/game/2048/'
